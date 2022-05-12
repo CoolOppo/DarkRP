c8167c5076dbd92d482a7559f9a2b373f130954b
CoolOppo <CoolOppo@users.noreply.github.com>
2022-05-12 04:35:35 -0400

    Make keys less retarded

--- entities/weapons/keys/shared.lua
+++ entities/weapons/keys/shared.lua
@@ -9,7 +9,6 @@ if CLIENT then
     SWEP.SlotPos = 1
     SWEP.DrawAmmo = false
     SWEP.DrawCrosshair = false
-
     include("cl_menu.lua")
 end
 
@@ -19,27 +18,21 @@ SWEP.Instructions = "Left click to lock\nRight click to unlock\nReload for door 
 SWEP.Contact = ""
 SWEP.Purpose = ""
 SWEP.IsDarkRPKeys = true
-
 SWEP.WorldModel = ""
-
 SWEP.ViewModelFOV = 62
 SWEP.ViewModelFlip = false
 SWEP.AnimPrefix = "rpg"
-
 SWEP.UseHands = true
-
 SWEP.Spawnable = true
 SWEP.AdminOnly = true
 SWEP.Category = "DarkRP (Utility)"
 SWEP.Sound = "doors/door_latch3.wav"
-
-SWEP.Primary.Delay = 0.3
+SWEP.Primary.Delay = 0.0
 SWEP.Primary.ClipSize = -1
 SWEP.Primary.DefaultClip = 0
 SWEP.Primary.Automatic = false
 SWEP.Primary.Ammo = ""
-
-SWEP.Secondary.Delay = 0.3
+SWEP.Secondary.Delay = 0.0
 SWEP.Secondary.ClipSize = -1
 SWEP.Secondary.DefaultClip = 0
 SWEP.Secondary.Automatic = false
@@ -52,6 +45,7 @@ end
 function SWEP:Deploy()
     if CLIENT or not IsValid(self:GetOwner()) then return true end
     self:GetOwner():DrawWorldModel(false)
+
     return true
 end
 
@@ -65,49 +59,37 @@ end
 
 local function lookingAtLockable(ply, ent, hitpos)
     local eyepos = ply:EyePos()
-    return IsValid(ent)
-        and ent:isKeysOwnable()
-        and (
-            ent:isDoor() and eyepos:DistToSqr(hitpos) < 2000
-            or
-            ent:IsVehicle() and eyepos:DistToSqr(hitpos) < 4000
-        )
+
+    return IsValid(ent) and ent:isKeysOwnable() and (ent:isDoor() and eyepos:DistToSqr(hitpos) < 2000 or ent:IsVehicle() and eyepos:DistToSqr(hitpos) < 4000)
 end
 
 local function lockUnlockAnimation(ply, snd)
-    ply:EmitSound("npc/metropolice/gear" .. math.random(1, 6) .. ".wav")
-    timer.Simple(0.9, function() if IsValid(ply) then ply:EmitSound(snd) end end)
+    if IsValid(ply) then
+        ply:EmitSound(snd)
+    end
 
     umsg.Start("anim_keys")
-        umsg.Entity(ply)
-        umsg.String("usekeys")
+    umsg.Entity(ply)
+    umsg.String("usekeys")
     umsg.End()
-
     ply:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_GMOD_GESTURE_ITEM_PLACE, true)
 end
 
 local function doKnock(ply, sound)
     ply:EmitSound(sound, 100, math.random(90, 110))
-
     umsg.Start("anim_keys")
-        umsg.Entity(ply)
-        umsg.String("knocking")
+    umsg.Entity(ply)
+    umsg.String("knocking")
     umsg.End()
-
     ply:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_HL2MP_GESTURE_RANGE_ATTACK_FIST, true)
 end
 
 function SWEP:PrimaryAttack()
     local Owner = self:GetOwner()
-
     if not IsValid(Owner) then return end
-
     local trace = Owner:GetEyeTrace()
-
     if not lookingAtLockable(Owner, trace.Entity, trace.HitPos) then return end
-
     self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
-
     if CLIENT then return end
 
     if Owner:canKeysLock(trace.Entity) then
@@ -122,15 +104,10 @@ end
 
 function SWEP:SecondaryAttack()
     local Owner = self:GetOwner()
-
     if not IsValid(Owner) then return end
-
     local trace = Owner:GetEyeTrace()
-
     if not lookingAtLockable(Owner, trace.Entity, trace.HitPos) then return end
-
     self:SetNextSecondaryFire(CurTime() + self.Secondary.Delay)
-
     if CLIENT then return end
 
     if Owner:canKeysUnlock(trace.Entity) then
@@ -145,10 +122,15 @@ end
 
 function SWEP:Reload()
     local trace = self:GetOwner():GetEyeTrace()
+
     if not IsValid(trace.Entity) or ((not trace.Entity:isDoor() and not trace.Entity:IsVehicle()) or self:GetOwner():EyePos():DistToSqr(trace.HitPos) > 40000) then
-        if CLIENT and not DarkRP.disabledDefaults["modules"]["animations"] then RunConsoleCommand("_DarkRP_AnimationMenu") end
+        if CLIENT and not DarkRP.disabledDefaults["modules"]["animations"] then
+            RunConsoleCommand("_DarkRP_AnimationMenu")
+        end
+
         return
     end
+
     if SERVER then
         umsg.Start("KeysMenu", self:GetOwner())
         umsg.End()
